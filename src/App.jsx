90edb70a7cf1db610af6df478e99d870526876e4
Tony Stagon <leelethabi@gmail.com>
2025-02-19 05:33:52 +0200

    About components with a test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import "./App.css";
+import React from 'react';
+import About from './components/About';
+import Test from './components/Test';
+import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <div className="content">
+        <nav>
+          <a href="#about">About</a> |  
+          <a href="#test">Test</a>
+        </nav>
+
+        <div className="sections">
+          <section id="about">
+            <About />
+          </section>
+          
+          <section id="test">
+            <Test />
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';  // 🔥 Already wrapping App
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>  
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>  
+);
