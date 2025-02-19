e809a51b7f0874589ddd01291174a44e3f8ed140
Tony Stagon <leelethabi@gmail.com>
2025-02-19 08:46:52 +0200

    first Pulled from components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react';
-import About from './components/About';
-import Test from './components/Test';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <div className="content">
-        <nav>
-          <a href="#about">About</a> |  
-          <a href="#test">Test</a>
-        </nav>
-
-        <div className="sections">
-          <section id="about">
-            <About />
-          </section>
-          
-          <section id="test">
-            <Test />
-          </section>
-        </div>
-      </div>
+    <div className="App">
+    
     </div>
   );
 }
