7ff0cd81799838776e0ed20b484925c6ece3edb7
RangoSS <tshidaisa@outlook.com>
2025-02-20 08:59:49 +0200

    category prefarances

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,9 @@
-import React, { useState } from "react";
-import CategoryPreferences from "./components/categoryPreferance/categoryPrefarances";
+import "./App.css";
 
-const App = () => {
-  const [submittedData, setSubmittedData] = useState(null);
 
-  const handleSubmit = (data) => {
-    setSubmittedData(data);
-    console.log("Form Data Submitted:", data);
-  };
-
-  return (
-    <div className="App">
-      
-      <CategoryPreferences onSubmit={handleSubmit} />
-
-      {submittedData && (
-        <div style={{ marginTop: "20px", padding: "10px", border: "1px solid black" }}>
-          <h3>Submitted Data:</h3>
-          <pre>{JSON.stringify(submittedData, null, 2)}</pre>
-        </div>
-      )}
-    </div>
-  );
-};
+function App() {
+  return <div className="App">
+  </div>;
+}
 
 export default App;
