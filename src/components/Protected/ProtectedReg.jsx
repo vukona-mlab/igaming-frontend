2bc601360f7d3f1712bc729e2890d930d7d467ab
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-21 22:47:50 +0200

    updated Protected route

--- src/components/Protected/ProtectedReg.jsx
+++ src/components/Protected/ProtectedReg.jsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function ProtectedRouteReg({ auth }) {
   const uid = localStorage.getItem("uid");
 
-  return uid !== null && uid !== "" ? <Navigate to="/home" /> : <Outlet />;
+  return uid !== null && uid !== "" ? <Navigate to="/profile" /> : <Outlet />;
 }
--- src/pages/freelancer/SigninForm/SignIn.jsx
+++ src/pages/freelancer/SigninForm/SignIn.jsx
@@ -4,11 +4,11 @@ import InputForm from "../../../components/Auth/reusable-input-form/InputForm";
 import LoginRegisterButton from "../../../components/Auth/LoginRegisterButton/LoginRegisterButton";
 import EmailResetButton from "../../../components/Auth/EmailResetButton/Button";
 import GoogleSignInButton from "../../../components/Auth/googleSignButton/googleSign";
-
+import { useNavigate } from "react-router";
 export default function SignIn() {
   const [formData, setFormData] = useState({ username: "", password: "" });
   const [error, setError] = useState("");
-
+  const navigation = useNavigate();
   function handleFormDataChange(e) {
     const value = e.target.value;
     setFormData(value);
@@ -20,10 +20,37 @@ export default function SignIn() {
     }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
+    //e.preventDefault();
+
     if (!error) {
       // Submit form
+      try {
+        const res = await fetch(`http://localhost:8000/api/auth/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formData.username,
+            password: formData.password,
+          }),
+        });
+        const data = await res.json();
+        if (res.ok) {
+          console.log({ data });
+          localStorage.setItem("token", data.token);
+          localStorage.setItem("uid", data.user.uid);
+          localStorage.setItem("role", data.user.roles[0]);
+
+          navigation("/profile");
+        }
+
+        //setLoading(false);
+      } catch (err) {
+        console.log(err);
+        //setLoading(false);
+      }
     }
   };
   return (
@@ -47,10 +74,7 @@ export default function SignIn() {
             label2={"password"}
           />
           <div className="email-button">
-            <EmailResetButton
-              text="Continue with email"
-              onClick={handleSubmit}
-            />
+            <EmailResetButton text="Continue with email" func={handleSubmit} />
           </div>
           <div className="google-signin">
             <GoogleSignInButton />
