93203b95895fc4baa37498ca2497a18ca1efd9d4
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-02-21 15:12:35 +0200

    Fixed app jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import NavBar from "./components/Navbar/navbar";
-import SubNavBar from "./components/SubNavBar/SubNavBar";
-import InputForm from "./components/reusable-input-form/InputForm";
-import SignIn from "./pages/freelancer/SignIn";
 
 
 function App() {
   return <div className="App">
-   <SignIn/>
+   
   </div>;
 }
 
