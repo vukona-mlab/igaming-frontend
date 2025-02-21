2441dfd24482abc048b2f2741e76925b3efce94e
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-02-21 15:59:08 +0200

    Updated the corrected code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import NavBar from "./components/Navbar/navbar";
+
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <NavBar/>
+  </div>;
 }
 
 export default App;
