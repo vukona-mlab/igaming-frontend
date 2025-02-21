ca7ac30e4eafb14cc840395e1e840d277a3191ee
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-21 19:21:09 +0200

    updated gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import React from "react";
 import "./App.css";
-import ProfilePage from "./pages/freelancer/freelanceProfile/frelancerProfile"; // Make sure the path is correct
 
 function App() {
-  return (
-    <div className="App">
-      <ProfilePage /> {/* Call ProfilePage component */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
