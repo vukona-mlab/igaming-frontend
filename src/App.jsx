f369284263255bd57d0793def7a0f8d13af87758
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-21 19:38:40 +0200

    resolved merge conflict - App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import React from "react";
 import "./App.css";
-import ClientPage from "./pages/clientProfile/clientProfile"; // Make sure the path is correct
 
 function App() {
-  return (
-    <div className="App">
-      <ClientPage /> {/* Call ProfilePage component */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
