08b30a87e31f26ff5878a00cbcdb0888e234d4a9
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-21 19:30:37 +0200

    resolved merge conflict - App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import "./App.css";
-import EmailResetButton from "./components/EmailResetButton/Button";
-
 
 function App() {
-  return <div className="App">
-    <EmailResetButton/>
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
