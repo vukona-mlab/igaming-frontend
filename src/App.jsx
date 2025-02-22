75ef50fbfe5106eb6d4a85bb2fe55130ea2e0733
Tony Stagon <leelethabi@gmail.com>
2025-02-22 09:27:16 +0200

    Forgot Password

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
 
 function App() {
   return <div className="App">
-
+<ForgotPassword/>
   </div>;
 }
 
 export default App;
+
---
+++ src/pages/forgotPassword/ForgotPassword.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CostumizeForgot  from "../../pages//forgotPassword/CostumizeForgot"; 
+
+
+function ForgotPassword() {
+  return (
+    <>
+ <CostumizeForgot />
+      
+    </>
+  );
+}
+
+export default ForgotPassword;
