0d755fcd858430fd8529b033091629f3afd71897
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-24 21:21:18 +0200

    added TopFreelancer and MadeOnRi sections

---
+++ src/components/Landing/TopFreelancers/TopFreelancers.jsx
@@ -0,0 +1,58 @@
+import Reac, { useState, useEffect } from "react";
+import styles from "./TopFreelancers.module.css";
+import SectionHeader from "../section-header/SectionHeader";
+import FreelancerCard from "../../Freelancer Card/FreelancerCard";
+import SeeMoreButton from "../SeeMoreButton/SeeMoreButton";
+
+const TopFreelancers = () => {
+  const [loading, setLoading] = useState(true);
+  const [freelancers, setFreelancers] = useState([]);
+  const url = "http://localhost:8000/api/freelancers/projects";
+
+  useEffect(() => {
+    getTopFreelancers();
+  }, []);
+
+  const getTopFreelancers = async () => {
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(response);
+
+        setFreelancers(data.freelancers.slice(0, 3));
+      }
+
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+  if (loading) return;
+  return (
+    <section className={styles.TopFreelancers}>
+      <SectionHeader text="Top Freelancers" />
+      <div className={styles.sectionBody}>
+        {freelancers &&
+          freelancers.map((freelancer, i) => (
+            <FreelancerCard
+              key={i}
+              profilePicture={freelancer.profilePicture}
+              name={freelancer.displayName}
+              jobTitle={freelancer.jobTitle}
+              projectsCompleted={freelancer.projects.length}
+              rating={freelancer.rating}
+            />
+          ))}
+      </div>
+      <div className={styles.sectionButton}>
+        <SeeMoreButton text="See more" onClick={() => console.log()} />
+      </div>
+    </section>
+  );
+};
+
+export default TopFreelancers;
--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -5,12 +5,16 @@ import SubNavBar from "../../components/Common/SubNavBar/SubNavBar";
 import About from "../../components/about/About";
 import Footer from "../footer/Footer";
 import Showcase from "../../components/Landing/Showcase/Showcase";
+import TopFreelancers from "../../components/Landing/TopFreelancers/TopFreelancers";
+import MadeOnRI from "../../components/Landing/MadeOnRI/MadeOnRI";
 function LandingPage() {
   return (
     <>
       <CustomNavBar />
       <SubNavBar />
-      <Showcase/>
+      <Showcase />
+      <TopFreelancers />
+      <MadeOnRI />
       <About />
       <Footer />
     </>
