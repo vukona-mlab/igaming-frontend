e1286b8f4a5f3d414cbcfac639731a307517fc0f
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-25 19:10:47 +0200

    updated section title

--- src/components/Landing/TestimonialsSection/TestifierCard.jsx
+++ src/components/Landing/TestimonialsSection/TestifierCard.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./TestifierCard.module.css";
 
 const TestifierCard = ({ name, country, date, imageUrl }) => {
+  let dateStr = new Date(date);
+  dateStr = dateStr.toJSON().slice(0, dateStr.toJSON().indexOf("T"));
+  dateStr = dateStr.replace(/\-/g, "/");
   return (
     <div className={styles.TestifierCard}>
       <img
@@ -11,9 +14,7 @@ const TestifierCard = ({ name, country, date, imageUrl }) => {
       <div className={styles.testifierDetails}>
         <div className={styles.testifierName}>{name}</div>
         <div className={styles.testifierCountry}>{country}</div>
-        <div className={styles.testifierDate}>
-          {new Date(date).toUTCString()}
-        </div>
+        <div className={styles.testifierDate}>{dateStr}</div>
       </div>
     </div>
   );
