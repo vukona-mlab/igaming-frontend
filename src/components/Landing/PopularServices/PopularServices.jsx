f652cdcd0cfae39fd35ec366defea6e123ca1765
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-25 16:00:22 +0200

    added section title to sections

--- src/components/Landing/PopularServices/PopularServices.jsx
+++ src/components/Landing/PopularServices/PopularServices.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import ServicesCard from "../../ServicesCard/ServicesCard"; 
+import ServicesCard from "../../ServicesCard/ServicesCard";
 import SectionHeader from "../section-header/SectionHeader";
 import SeeMoreButton from "../SeeMoreButton/SeeMoreButton";
 import "./PopularServices.css";
+import SectionTitle from "../SectionTitle/SectionTitle";
 
 const PopularServices = () => {
   const [services, setServices] = useState([]);
@@ -35,22 +36,29 @@ const PopularServices = () => {
   return (
     <section className="popular-services">
       <SectionHeader text="Popular Services" />
+      <SectionTitle
+        title="Most searched"
+        subtitle="Across all the categeries that we have , these are the most searched categories"
+        span="categories"
+      />
       <div className="services-container">
         {services.map((service) => (
           <div key={service.id} className="service-card">
             <ServicesCard
-              profilePicture={service.image || "https://via.placeholder.com/150"}
+              profilePicture={
+                service.image || "https://via.placeholder.com/150"
+              }
               name={service.name}
               projectsCompleted={service.projects || 0}
               likes={service.likes || 0}
-              onMessageClick={() => console.log(`Explore ${service.name}`)} 
-              buttonText="Explore" 
+              onMessageClick={() => console.log(`Explore ${service.name}`)}
+              buttonText="Explore"
             />
           </div>
         ))}
       </div>
       <div className="sectionButton">
-      <SeeMoreButton text="See more" onClick={() => console.log()} />
+        <SeeMoreButton text="See more" onClick={() => console.log()} />
       </div>
     </section>
   );
