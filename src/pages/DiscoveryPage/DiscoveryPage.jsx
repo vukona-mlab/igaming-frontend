cd80f08cb5ea25007a2cf506c295d7f6f89c6814
Jesse <jesse.mashoana@gmail.com>
2025-02-25 10:00:21 +0200

    completed freelancer discoevry section

---
+++ src/pages/DiscoveryPage/DiscoveryPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import NavBar from '../../components/Common/Navbar/navbar';
+import SubNavBar from '../../components/Common/SubNavBar/SubNavBar';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import FreelancerDiscovery from '../../components/FreelancerDiscovery/FreelancerDiscovery';
+import './DiscoveryPage.css';
+
+const DiscoveryPage = () => {
+  return (
+    <div className="discovery-page">
+      <div className="navigation-container">
+        <NavBar />
+        <SubNavBar />
+      </div>
+      
+      <div className="content-container">
+        <SearchBar placeholder="Search freelancers..." />
+        <FreelancerDiscovery />
+      </div>
+    </div>
+  );
+};
+
+export default DiscoveryPage; 
