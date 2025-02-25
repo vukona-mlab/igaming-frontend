9c06dbae2197fb7e8db1834d30a5ee42c8dd3413
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-25 13:48:01 +0200

    updated Auth pages

--- src/components/Auth/LoginRegisterButton/LoginRegisterButton.jsx
+++ src/components/Auth/LoginRegisterButton/LoginRegisterButton.jsx
@@ -1,15 +1,15 @@
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { HiArrowSmRight } from "react-icons/hi";
 
+import "./loginButton.css";
 
-
-import './loginButton.css';
-
-function LoginRegisterButton({text, func}) {
-
+function LoginRegisterButton({ text, func }) {
   return (
     <>
-      <Button className="login-register-button" variant="dark" onClick={func}>{text}<HiArrowSmRight className='arrow-icon'/></Button>
+      <Button className="login-register-button" variant="dark" onClick={func}>
+        {text}
+        <HiArrowSmRight className="arrow-icon" />
+      </Button>
     </>
   );
 }
