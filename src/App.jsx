e1efc9f4bcfb4361786053a8af0a41f8fa985038
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-02-25 14:18:39 +0200

    Worked on Get Started Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ResetPassword from "./pages/ResetPassword/ResetPassword";
-import FreelancerRegister from "./pages/freelancer/Register-page/Register";
-import FreelancerSignIn from "./pages/freelancer/SigninForm/SignIn";
-import FreelancerProfile from "./pages/freelancer/freelanceProfile/frelancerProfile";
+import GetStarted from "./pages/GetStartedSection/GetStarted";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import ResetPassword from "./pages/ResetPassword/ResetPassword";
+// import FreelancerRegister from "./pages/freelancer/Register-page/Register";
+// import FreelancerSignIn from "./pages/freelancer/SigninForm/SignIn";
+// import FreelancerProfile from "./pages/freelancer/freelanceProfile/frelancerProfile";
 
-import ClientProfile from "./pages/client/clientProfile/clientProfile";
-import ClientRegister from "./pages/client/Register-page/ClientRegister";
-import ClientSignIn from "./pages/client/SignInPage/SignIn";
-import LandingPage from "./pages/landingPage/LandingPage";
-import ProtectedRouteReg from "./components/Protected/ProtectedReg";
-import ProtectedRoutes from "./components/Protected/ProtectedRoutes";
-import ProtectRole from "./components/Protected/ProtectRole";
+// import ClientProfile from "./pages/client/clientProfile/clientProfile";
+// import ClientRegister from "./pages/client/Register-page/ClientRegister";
+// import ClientSignIn from "./pages/client/SignInPage/SignIn";
+// import LandingPage from "./pages/landingPage/LandingPage";
+// import ProtectedRouteReg from "./components/Protected/ProtectedReg";
+// import ProtectedRoutes from "./components/Protected/ProtectedRoutes";
+// import ProtectRole from "./components/Protected/ProtectRole";
+
 
 function App() {
   const userRole = localStorage.getItem("role");
   return (
     <div className="App">
-      <Router>
+      <GetStarted/>
+      {/* <Router>
         <div className="App">
           <Routes>
             <Route exact path="/" element={<LandingPage />} />
@@ -44,7 +47,7 @@ function App() {
             </Route>
           </Routes>
         </div>
-      </Router>
+      </Router> */}
     </div>
   );
 }
