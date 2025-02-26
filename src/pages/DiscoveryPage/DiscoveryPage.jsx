056e559228d122bd8d44a16d3aec7d6649024d64
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-26 19:11:27 +0200

    updated auth page routing

--- src/pages/DiscoveryPage/DiscoveryPage.jsx
+++ src/pages/DiscoveryPage/DiscoveryPage.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import NavBar from '../../components/Common/Navbar/navbar';
-import SubNavBar from '../../components/Common/SubNavBar/SubNavBar';
-import SearchBar from '../../components/SearchBar/SearchBar';
-import FreelancerDiscovery from '../../components/FreelancerDiscovery/FreelancerDiscovery';
-import './DiscoveryPage.css';
+import React, { useState } from "react";
+import NavBar from "../../components/Common/Navbar/navbar";
+import SubNavBar from "../../components/Common/SubNavBar/SubNavBar";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import FreelancerDiscovery from "../../components/FreelancerDiscovery/FreelancerDiscovery";
+import "./DiscoveryPage.css";
 
 const DiscoveryPage = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
 
   const handleSearch = (query) => {
     setSearchQuery(query);
@@ -19,8 +19,8 @@ const DiscoveryPage = () => {
         <SubNavBar />
       </div>
       <div className="search-container">
-        <SearchBar 
-          placeholder="Search by name or job title..." 
+        <SearchBar
+          placeholder="Search by name or job title..."
           onSearch={handleSearch}
         />
       </div>
@@ -33,4 +33,4 @@ const DiscoveryPage = () => {
   );
 };
 
-export default DiscoveryPage; 
+export default DiscoveryPage;
