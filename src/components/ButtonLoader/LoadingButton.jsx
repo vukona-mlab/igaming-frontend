a47e7e46f3cdd942501b9237685a0dc3de213b9a
Tony Stagon <leelethabi@gmail.com>
2025-02-26 14:47:21 +0200

    Button Spinner Updated

--- src/components/ButtonLoader/LoadingButton.jsx
+++ src/components/ButtonLoader/LoadingButton.jsx
@@ -15,9 +15,9 @@ const LoadingButton = ({ onClick, text }) => {
 
   return (
     <button onClick={handleClick} className="loading-button" disabled={loading}>
-      {loading ? (
+      { loading ? (
         <img
-          src="/public/images/spinDash.gif" // Path to your GIF
+          src="/public/images/load.gif" // Path to your GIF
           alt="Loading"
           className="loader-gif" // Add a class for styling if needed
           style={{ width: "20px", height: "20px" }} // Adjust size here
