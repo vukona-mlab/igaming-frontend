c776bea89487bb6f09cd1ce08642d6528421aaa9
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-02-27 11:36:52 +0200

    added getStarted section

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -11,15 +11,15 @@ import MadeOnRI from "../../components/Landing/MadeOnRI/MadeOnRI";
 import FAQSection from "../../components/Landing/FAQSection/FAQSection";
 import SpeechBubble from "../../components/Landing/TestimonialsSection/SpeechBubble";
 import TestimonialsSection from "../../components/Landing/TestimonialsSection/TestimonialsSection";
+import GetStarted from "../../components/Landing/GetStartedSection/GetStarted";
 function LandingPage() {
-  const userRole = localStorage.getItem("role");
-
   return (
     <>
       <NavBar />
       <SubNavBar />
       <Showcase />
       <About />
+      <GetStarted />
       <TopFreelancers />
       <PopularServices />
       <MadeOnRI />
