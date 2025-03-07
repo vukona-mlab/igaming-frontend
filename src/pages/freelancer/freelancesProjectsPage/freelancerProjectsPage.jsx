07985f03b0f712089b7c2941bde48b9a9b4f98ef
RangoSS <tshidaisa@outlook.com>
2025-03-07 14:54:51 +0200

    updating collection

--- src/pages/freelancer/freelancesProjectsPage/freelancerProjectsPage.jsx
+++ src/pages/freelancer/freelancesProjectsPage/freelancerProjectsPage.jsx
@@ -14,7 +14,7 @@ const FreelancerProjects = () => {
   });
   const { freelancer_id } = useParams();
   const freelancerIdToShow = freelancer_id || "5Ei0TkC4TUblQ0NVHN46PupY3w83"; // Hardcoded freelancer ID
-  ///////
+
   useEffect(() => {
     fetch("http://localhost:8000/api/freelancers/projects")
       .then((response) => response.json())
@@ -63,43 +63,49 @@ const FreelancerProjects = () => {
   return (
     <>
       <Navbar />
-      <Container>
-        <h1 className="text-center my-4">Freelancer</h1>
-        <Row>
-          {/* Freelancer Profile Section - Always on Top */}
-          <Col md={3} className="">
-            <FreelancerProjectCards
-              image={freelancerData.profilePicture}
-              specialities={freelancerData.specialities}
-            />
-          </Col>
+      {projects.length === 0 ? (
+        <div className="text-center my-4">
+          <h3>Oops! Freelancer currently does not have projects to display.</h3>
+        </div>
+      ) : (
+        <Container>
+          <h1 className="text-center my-4">Freelancer</h1>
+          <Row>
+            {/* Freelancer Profile Section - Always on Top */}
+            <Col md={3}>
+              <FreelancerProjectCards
+                image={freelancerData.profilePicture}
+                specialities={freelancerData.specialities}
+              />
+            </Col>
 
-          {/* Projects Section - Below Profile on Small Screens */}
-          <Col md={9} className="">
-            <Row className="g-2">
-              {projects.slice(0, visibleProjects).map((project) => (
-                <Col key={project.id} md={4}>
-                  <ProjectCard
-                    projectPicture={project.projectPicture}
-                    projectName={project.projectName}
-                    likes={project.likes}
-                    authorName={project.author}
-                    onDemoClick={() => window.open(project.demoLink, "_blank")}
-                    onShareClick={() => alert(`Sharing ${project.projectName}`)}
-                  />
-                </Col>
-              ))}
-            </Row>
-            {visibleProjects < projects.length && (
-              <div className="text-center mt-4">
-                <Button variant="dark" onClick={handleSeeMore}>
-                  See More
-                </Button>
-              </div>
-            )}
-          </Col>
-        </Row>
-      </Container>
+            {/* Projects Section - Below Profile on Small Screens */}
+            <Col md={9}>
+              <Row className="g-2">
+                {projects.slice(0, visibleProjects).map((project) => (
+                  <Col key={project.id} md={4}>
+                    <ProjectCard
+                      projectPicture={project.projectPicture}
+                      projectName={project.projectName}
+                      likes={project.likes}
+                      authorName={project.author}
+                      onDemoClick={() => window.open(project.demoLink, "_blank")}
+                      onShareClick={() => alert(`Sharing ${project.projectName}`)}
+                    />
+                  </Col>
+                ))}
+              </Row>
+              {visibleProjects < projects.length && (
+                <div className="text-center mt-4">
+                  <Button variant="dark" onClick={handleSeeMore}>
+                    See More
+                  </Button>
+                </div>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      )}
     </>
   );
 };
