20b64be457fbaed98f9f3eafd73e81b24f575d19
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-10 18:50:02 +0200

    updated banking card styles

--- src/components/Payments/BankingCard/BankingCard.jsx
+++ src/components/Payments/BankingCard/BankingCard.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./BankingCard.css";
 
-const BankingCard = ({ bankName, accountNumber }) => {
+const BankingCard = ({ bankName, accountNumber, func }) => {
   return (
-    <div className="BankingCard">
+    <div className="BankingCard" onClick={func}>
       <div className="bc-header">
         <div className="bc-bank-name">{bankName}</div>
         <button className="bc-edit">Edit</button>
       </div>
       <div className="bc-acc-num">
         <div className="bc-acc-num-name">Account Number:</div>
-        <div className="bc-acc-num-val">{accountNumber}</div>
+        <div className="bc-acc-num-val">
+          {accountNumber.replace(/^.{12}/g, "************")}
+        </div>
       </div>
     </div>
   );
