da5b81fc16c21be730e3375c125a574d387226e3
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-10 19:54:52 +0200

    added transactions section

---
+++ src/components/Payments/TransactionsSection/TransactionsSection.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TransactionsSection.css";
+
+const TransactionsSection = ({}) => {
+  return <div className="TransactionsSection"></div>;
+};
+
+export default TransactionsSection;
