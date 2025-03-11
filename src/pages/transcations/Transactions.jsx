3c2d2fe8b876d6e68f56de2d640a4bad8dec1383
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-11 12:48:32 +0200

    added transaction page

--- src/pages/transcations/Transactions.jsx
+++ src/pages/transcations/Transactions.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Transactions.css";
-
+import Navbar from "../../components/Common/Navbar/navbar";
+import ProfileSubNav from "../../components/Profile/ProfileSubNav/ProfileSubNav";
+import TransactionsSection from "../../components/Payments/TransactionsSection/TransactionsSection";
+import TabsHeader from "../../components/Payments/TabsHeader/TabsHeader";
 const Transactions = () => {
-  return <div className="Transactions"></div>;
+  const [tab, setTab] = useState("Banking Details");
+  return (
+    <div className="Transactions">
+      <Navbar />
+      <ProfileSubNav />
+      <div className="transaction-page-sections">
+        <TabsHeader
+          fieldOne="Banking Details"
+          fieldTwo="Transactions"
+          handleTabChange={setTab}
+        />
+        {tab === "Banking Details" ? <div></div> : <TransactionsSection />}
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
