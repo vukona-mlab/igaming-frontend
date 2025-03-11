31c438dc768836f82af424b715ad98ba14569294
MabohlaleNkuna <111710433+MabohlaleNkuna@users.noreply.github.com>
2025-03-11 11:29:25 +0200

    Add props to AddBankDetailsForm for customizable bank selection and submission handling

--- src/components/Payments/AddDetailsForm/AddDetails.jsx
+++ src/components/Payments/AddDetailsForm/AddDetails.jsx
@@ -2,9 +2,13 @@ import React, { useState } from "react";
 import "./AddDetails.css"; 
 import arrowDropDown from "../../../../public/images/arrow_drop_down.png"; 
 
-const AddBankDetailsForm = () => {
+const AddBankDetailsForm = ({ 
+  initialBank = "Choose bank", 
+  onSubmit,
+  onCancel 
+}) => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedBank, setSelectedBank] = useState("Choose bank");
+  const [selectedBank, setSelectedBank] = useState(initialBank);
 
   const handleDropdownClick = () => {
     setIsOpen(!isOpen);
@@ -17,30 +21,63 @@ const AddBankDetailsForm = () => {
 
   return (
     <div className="flex flex-col items-center w-full max-w-[400px] mx-auto">
+      {/* Form heading */}
       <h2 className="text-gray-500 text-lg mb-4">Add Bank Details</h2>
-      <form className="w-[387.25px]">
+      <form className="w-[387.25px]" onSubmit={(e) => { 
+        e.preventDefault(); 
+        onSubmit({ selectedBank }); 
+      }}>
+
         <div className="relative">
-          <div className="w-full h-[33px] border-b border-gray-400 outline-none mb-4 custom-dropdown" onClick={handleDropdownClick}>
-            <span>{selectedBank}</span>
+          <div 
+            className="w-full h-[33px] border-b border-gray-400 outline-none mb-4 custom-dropdown" 
+            onClick={handleDropdownClick}>
+            <span>{selectedBank}</span> 
             <img
               src={arrowDropDown}
               alt="dropdown arrow"
               className={`absolute right-2 top-1/2 transform -translate-y-1/2 w-4 h-4 ${isOpen ? 'rotate-180' : ''}`}
             />
           </div>
+        
           {isOpen && (
             <div className="absolute w-full bg-white border border-gray-400 mt-1 rounded-md z-10">
               <div className="p-2 cursor-pointer" onClick={() => handleOptionClick("Bank A")}>mabohlale1</div>
               <div className="p-2 cursor-pointer" onClick={() => handleOptionClick("Bank B")}>mabohlale2</div>
-           
             </div>
           )}
         </div>
-        <input type="text" placeholder="Branch code" className="w-full h-[33px] border-b border-gray-400 outline-none mb-4" />
-        <input type="text" placeholder="Account number" className="w-full h-[33px] border-b border-gray-400 outline-none mb-6" />
+
+        {/* Branch Code Input */}
+        <input 
+          type="text" 
+          placeholder="Branch code" 
+          className="w-full h-[33px] border-b border-gray-400 outline-none mb-4" 
+        />
+
+        {/* Account Number Input */}
+        <input 
+          type="text" 
+          placeholder="Account number" 
+          className="w-full h-[33px] border-b border-gray-400 outline-none mb-6" 
+        />
+
+        {/* Buttons */}
         <div className="flex justify-between">
-          <button className="w-[192.5px] h-[57px] text-red-400 cancel">Cancel</button>
-          <button className="w-[192.5px] h-[57px] bg-black text-white submit">Submit</button>
+          <button 
+            type="button" 
+            className="w-[192.5px] h-[57px] text-red-400 cancel" 
+            onClick={onCancel} 
+          >
+            Cancel
+          </button>
+        
+          <button 
+            type="submit" 
+            className="w-[192.5px] h-[57px] bg-black text-white submit"
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>
