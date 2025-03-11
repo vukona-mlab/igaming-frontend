ed07dedcc820ff2fef13da3595eb949c0dd0cf31
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2025-03-11 12:48:32 +0200

    added transaction page

--- src/components/Payments/TransactionHistory/TransactionHistory.jsx
+++ src/components/Payments/TransactionHistory/TransactionHistory.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./TransactionHistory.css";
-
-const TransactionHistory = ({ totalTransferAmount }) => {
+import TransactionDetails from "../TransactionDetails/TransactionDetails";
+const TransactionHistory = ({ totalAmount }) => {
+  const role = localStorage.getItem("role");
   return (
     <div className="TransactionHistory">
       <div className="th-heading-value">
-        <div className="th-heading">Total Transfer</div>
-        <div className="th-value">ZAR R{totalTransferAmount}</div>
+        <div className="th-heading">
+          Total {role === "client" ? "Transfer" : "Earned"}
+        </div>
+        <div className="th-value">ZAR R{totalAmount}</div>
       </div>
-      <div className="th-title">Transfer History</div>
-      <div className="th-transactions-div"></div>
+      <div className="th-title">
+        {role === "client" ? "Transfer" : "Earning"} History
+      </div>
+      <div className="th-transactions-div">
+        <TransactionDetails
+          message="You have transferred R70 000 to Ri Experts for Roulette Game from STANDARD BANK account *********5564;
+"
+          reference="REF 123F45568776UJ"
+          date="31 January 2024"
+        />
+      </div>
     </div>
   );
 };
