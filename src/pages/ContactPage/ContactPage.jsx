3a03edf974040d658d1a10ac5d1a11dfa6290eac
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2025-03-28 01:49:57 +0200

    Added css for the contact page

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -25,15 +25,17 @@ export default function ContactPage() {
           </div>
           <div className="contact-details">
             <ul>
-              <li><BiSolidPhoneCall />0660850741</li>
-              <li><MdEmail />igaming@gmail.com</li>
+              <li><BiSolidPhoneCall className="contact-icon"/>0660850741</li>
+              <li><MdEmail className="contact-icon"/>igaming@gmail.com</li>
               <li>
-              <IoLocationSharp /> 132 igaming Street JHB,
+              <IoLocationSharp className="contact-icon"/> 132 igaming Street JHB,
                 <br />
                 Massachusetts 02156 South Africa
               </li>
             </ul>
-            <div className="social-links"><IoLogoTwitter /><SlSocialInstagram /><FaDiscord /></div>
+            <div className="social-links"><IoLogoTwitter className="social-icon"/><SlSocialInstagram className="social-icon"/><FaDiscord className="social-icon"/></div>
+            <img src="/public/images/Ellipse-full.png" alt="full ellipse" className="full-ellipse"></img>
+            <img src="/public/images/Ellipse-half.png" alt="half ellipse" className="half-ellipse"></img>
           </div>
         </div>
         <div className="right">
@@ -41,29 +43,29 @@ export default function ContactPage() {
           <div className="input-container">
             <div className="input-left">
               {/* <form> */}
-                <label htmlFor="fname">First name:</label>
+                <label htmlFor="fname">First name</label>
                 <br />
-                <input type="text" id="fname" name="fname" />
+                <input type="text" id="fname" name="fname" className="input-field" />
                 <br />
-                <label htmlFor="lname">Email:</label>
+                <label htmlFor="lname">Email</label>
                 <br />
-                <input type="text" id="lname" name="lname" />
+                <input type="text" id="lname" name="lname" className="input-field"/>
               {/* </form> */}
             </div>
             <div className="input-right">
               {/* <form> */}
-                <label htmlFor="fname">Last name:</label>
+                <label htmlFor="fname">Last name</label>
                 <br />
-                <input type="text" id="fname" name="fname" />
+                <input type="text" id="fname" name="fname" className="input-field"/>
                 <br />
-                <label htmlFor="lname">Phone number:</label>
+                <label htmlFor="lname">Phone number</label>
                 <br />
-                <input type="text" id="lname" name="lname" />
+                <input type="text" id="lname" name="lname" className="input-field"/>
               {/* </form> */}
             </div>
           </div>
           <div className="subjects">
-            <h6>Select subject?</h6>
+            <h6 className="subject-heading">Select subject?</h6>
             <div className="select-subjects">
               {/* <form className="check-form"> */}
                 <input
@@ -111,7 +113,7 @@ export default function ContactPage() {
                 style={{ height: "200px" }}
               />
             {/* </form> */}
-            <button>Send Message</button>
+            <div className="button">Send Message</div>
             </div>
           </div>
           </form>
